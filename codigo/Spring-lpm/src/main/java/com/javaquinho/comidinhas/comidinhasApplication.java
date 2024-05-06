894f66c039bd5eba97e945e3f06942ef1345c712
Eduardo <eduardo.henriquems@gmail.com>
2024-05-06 08:15:42 -0300

    adicionando o springboot

---
+++ codigo/Spring-lpm/src/main/java/com/javaquinho/comidinhas/comidinhasApplication.java
@@ -0,0 +1,13 @@
+package com.javaquinho.comidinhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class comidinhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(comidinhasApplication.class, args);
+	}
+
+}
---
+++ codigo/Spring-lpm/src/test/java/com/javaquinho/comidinhas/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaquinho.comidinhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class comidinhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
