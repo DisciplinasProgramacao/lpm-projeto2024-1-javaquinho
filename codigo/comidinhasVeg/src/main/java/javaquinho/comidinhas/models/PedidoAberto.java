2ef8611f78b206540c4714b1a180e9b0df5ca24e
iannovais <iandosreisnovais@gmail.com>
2024-06-23 11:43:02 -0300

    Acrescentada documentação #22

--- codigo/comidinhasVeg/src/main/java/javaquinho/comidinhas/models/PedidoAberto.java
+++ codigo/comidinhasVeg/src/main/java/javaquinho/comidinhas/models/PedidoAberto.java
@@ -4,20 +4,46 @@ import jakarta.persistence.Entity;
 import javaquinho.comidinhas.excecoes.MenuInvalidoException;
 import javaquinho.comidinhas.excecoes.NaoExisteMenuException;
 import javaquinho.comidinhas.excecoes.ProdutoNaoExisteNoMenuException;
+
+/**
+ * Classe representando um PedidoAberto, que é um tipo específico de Pedido.
+ */
 @Entity
 public class PedidoAberto extends Pedido<MenuAberto> {
+
+    /**
+     * Construtor que inicializa a quantidade de pessoas.
+     *
+     * @param qntPessoas quantidade de pessoas
+     */
     public PedidoAberto(int qntPessoas){
         super(qntPessoas);
     }
 
+    /**
+     * Construtor que inicializa a quantidade de pessoas e o menu.
+     *
+     * @param qntPessoas quantidade de pessoas
+     * @param menu menu associado ao pedido
+     * @throws MenuInvalidoException se o menu for inválido
+     */
     public PedidoAberto(int qntPessoas, MenuAberto menu) throws MenuInvalidoException{
         super(qntPessoas, menu);
     }
 
+    /**
+     * Construtor padrão.
+     */
     public PedidoAberto(){
         super();
     }
     
+    /**
+     * Define o menu associado ao pedido.
+     *
+     * @param menu menu a ser associado
+     * @throws MenuInvalidoException se o menu for inválido
+     */
     @Override
     public void setMenu(MenuAberto menu) throws MenuInvalidoException{
         try {
@@ -27,6 +53,11 @@ public class PedidoAberto extends Pedido<MenuAberto> {
         }
     }
 
+    /**
+     * Retorna o valor total do pedido somando os preços dos produtos.
+     *
+     * @return valor total do pedido
+     */
     @Override
     public double getSomarTotal() {
         return getProdutos().stream()
@@ -34,6 +65,13 @@ public class PedidoAberto extends Pedido<MenuAberto> {
                .sum();
     }
 
+    /**
+     * Adiciona um produto ao pedido.
+     *
+     * @param produto produto a ser adicionado
+     * @throws NaoExisteMenuException se não houver menu associado ao pedido
+     * @throws ProdutoNaoExisteNoMenuException se o produto não existir no menu
+     */
     @Override
     public void addProduto(Produto produto) throws NaoExisteMenuException, ProdutoNaoExisteNoMenuException{
         if (this.getMenu() == null){
