b021ac839c6835ae4f239d6d55eb225714519578
roberta sophia <robertasophiacsilva@gmail.com>
2024-06-23 14:09:57 -0300

    model do produto documentado

--- codigo/comidinhasVeg/src/main/java/javaquinho/comidinhas/models/Produto.java
+++ codigo/comidinhasVeg/src/main/java/javaquinho/comidinhas/models/Produto.java
@@ -17,14 +17,23 @@ public class Produto {
 
     public static final String TABLE_NAME = "produto";
 
+    /**
+     * ID do produto
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true, nullable = false)
     private Long id;
 
+    /**
+     * Nome do produto
+     */
     @Column(name = "nome", nullable = false)
     private String nome;
 
+    /**
+     * Preço do produto
+     */
     @Column(name = "preco", nullable = false)
     private double preco;
 }
